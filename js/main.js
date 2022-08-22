fbe7d7053dbddd2841a9c9c807bfd8affcee0877
Yan Pablo <93152439+YanppSilva@users.noreply.github.com>
2022-08-22 13:23:21 -0300

    Primeiros passos do JS.
    Captura de elementos, envio do formulário para a página e extração do evento a partir do imput.

--- js/main.js
+++ js/main.js
@@ -1,5 +1,16 @@
 // captura dos elementos(formulario)
 const form = document.getElementById("novoItem")
 
+// envia o form para a página(acaba não entrando na função)
+form.addEventListener("submit", (evento) => {
+    // interrompe o envio do form para o navegador
+    evento.preventDefault()
+
+    console.log(evento)
+    // extrair o evento a partir do imput
+    console.log(evento.target.elements['nome'].value)    
+    console.log(evento.target.elements['quantidade'].value)
+})
+
 
 
