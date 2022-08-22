575398ea82daa5780a03ad9b16875a81cb697cac
Yan Pablo <93152439+YanppSilva@users.noreply.github.com>
2022-08-22 15:22:06 -0300

    Criando as validações
    Implementando a primeira função do projeto que retorna os dados passados em uma lista e estilizados com elemento HTML.

--- js/main.js
+++ js/main.js
@@ -1,16 +1,30 @@
 // captura dos elementos(formulario)
 const form = document.getElementById("novoItem")
+const lista = document.getElementById("lista")
 
 // envia o form para a página(acaba não entrando na função)
 form.addEventListener("submit", (evento) => {
     // interrompe o envio do form para o navegador
     evento.preventDefault()
 
-    console.log(evento)
-    // extrair o evento a partir do imput
-    console.log(evento.target.elements['nome'].value)    
-    console.log(evento.target.elements['quantidade'].value)
+    // cria elemento toda vez que o formulario for submetido, a funcao é acionada.
+    criaElemento(evento.target.elements['nome'].value, evento.target.elements['quantidade'].value)
 })
+
+function criaElemento(nome, quantidade) {
+   
+    // cria novo item 
+    const novoItem = document.createElement('li')
+    novoItem.classList.add("item")
+
+    const numeroItem = document.createElement('strong')
+    numeroItem.innerHTML = quantidade
+
+    //insere um elemento criado dentro do outro (novoItem + numeroItem)
+    novoItem.appendChild(numeroItem)
+    novoItem.innerHTML += nome
+    lista.appendChild(novoItem)
+}
 
 
 
